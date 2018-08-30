3c4e4423839cb8ef4354ca92196df7c95a631ab7
jbhensley <jbhensley@users.noreply.github.com>
2018-08-30 10:09:51 -0700

    Initial project add

---
+++ src/HuffmanTest/HuffmanDecodingException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HuffmanTest
+{
+    // TODO: Should this be public?
+    internal class HuffmanDecodingException : Exception
+    {
+        public HuffmanDecodingException()
+        {
+        }
+    }
+}
---
+++ src/HuffmanTest/Program.cs
@@ -0,0 +1,44 @@
+﻿using BenchmarkDotNet.Running;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Xunit;
+
+namespace HuffmanTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<HuffmanTests>();
+
+            //var tests = new HuffmanTests();
+            //tests.SetupPerfTest();
+            //tests.ProcessHeadersWithLoop();
+            //tests.ProcessHeadersWithLoopAndComputedCodeMax();
+            //tests.ProcessHeadersWithDictionary();
+
+            //Console.WriteLine("Finished");
+            //Console.ReadLine();
+        }
+
+        static void ComputeCodeMax()
+        {
+            int codeMax = 0;
+            var s_decodingTable = Huffman.s_decodingTable;
+            for (int i = 0; i < s_decodingTable.Length; i++)
+            {
+                (int codeLength, int[] codes) = s_decodingTable[i];
+
+                if (i > 0)
+                {
+                    codeMax <<= codeLength - s_decodingTable[i - 1].codeLength;
+                }
+
+                codeMax += codes.Length;
+
+                Console.WriteLine($"{i}:\t{codeMax}");
+            }
+        }
+    }
+}
