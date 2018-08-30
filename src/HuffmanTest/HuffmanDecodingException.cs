5831f54b84be197a79eb4ad368cb54ddcf23db3b
Grant Dickinson <grant@k2.com>
2018-08-30 14:49:44 -0700

    Add grant-d algorithms

--- src/HuffmanTest/HuffmanDecodingException.cs
+++ src/HuffmanTest/HuffmanDecodingException.cs
@@ -1,13 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
 
 namespace HuffmanTest
 {
-    // TODO: Should this be public?
-    internal class HuffmanDecodingException : Exception
+    /// <summary>
+    /// 
+    /// </summary>
+    public class HuffmanDecodingException : Exception
     {
+        /// <summary>
+        /// 
+        /// </summary>
         public HuffmanDecodingException()
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="message"></param>
+        public HuffmanDecodingException(string message) : base(message)
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="innerException"></param>
+        public HuffmanDecodingException(string message, Exception innerException) : base(message, innerException)
         {
         }
     }
