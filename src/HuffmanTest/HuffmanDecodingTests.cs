309e8fb7a1e9f6883fc8444eeb3585cdc8178967
jbhensley <jbhensley@users.noreply.github.com>
2018-09-08 09:58:03 -0700

    Added tests for EOS in the middle of character combinations

--- src/HuffmanTest/HuffmanDecodingTests.cs
+++ src/HuffmanTest/HuffmanDecodingTests.cs
@@ -29,7 +29,7 @@ namespace HuffmanTest
             return s_decodeDelegate(source, destination);
         }
 
-        private static readonly (uint code, int bitLength)[] s_encodingTable = new(uint code, int bitLength)[]
+        public static readonly (uint code, int bitLength)[] s_encodingTable = new(uint code, int bitLength)[]
         {
             (0b11111111_11000000_00000000_00000000, 13),
             (0b11111111_11111111_10110000_00000000, 23),
@@ -446,8 +446,48 @@ namespace HuffmanTest
             var EOS = new byte[] { 0b11111111, 0b11111111, 0b11111111, 0b11111100 };
             yield return new object[] { EOS };
 
-            // TODO: send combinations with EOS in the middle
-            
+            // send combinations with EOS in the middle
+            destination = new byte[24];
+            for (int i = 0; i < 255; i++)
+            {   
+                ulong currentBits = 0;  // We can have 7 bits of rollover plus 30 bits for the next encoded value, so use a ulong
+                int currentBitCount = 0;
+                int dstOffset = 0;
+
+                ulong code = 0;
+                int bitLength = 0;
+                for (int j = 0; j < 3; j++)
+                {
+                    if (j == 1)
+                    {
+                        // hard-coded EOS on the second pass
+                        code = (ulong)(0b11111111_11111111_11111111_11111100) << 32;
+                        bitLength = 30;
+                    }
+
+                    else
+                        (code, bitLength) = HuffmanDecodingTests.GetEncodedValue(source[0]);
+
+                    currentBits |= code >> currentBitCount;
+                    currentBitCount += bitLength;
+
+                    while (currentBitCount >= 8)
+                    {
+                        destination[dstOffset++] = (byte)(currentBits >> 56);
+                        currentBits = currentBits << 8;
+                        currentBitCount -= 8;
+                    }
+                }
+
+                // Fill any trailing bits with ones, per RFC
+                if (currentBitCount > 0)
+                {
+                    currentBits |= 0xFFFFFFFFFFFFFFFF >> currentBitCount;
+                    destination[dstOffset++] = (byte)(currentBits >> 56);
+                }
+
+                yield return new object[] { destination.Take(dstOffset).ToArray() };
+            }
         }
     }
 }
--- src/HuffmanTest/Program.cs
+++ src/HuffmanTest/Program.cs
@@ -19,25 +19,50 @@ namespace HuffmanTest
             //DumpDecodingArray();
 
 
-            //0b11111111_11111111_11111111_11111100 // EOS
-            //var EOS = new byte[] { 0b11111111, 0b11111111, 0b11111111, 0b11111100 };
-            // a character followed by EOS
-            //var original = new byte[] { (byte)'a' }.Concat(EOS).ToArray();
-            //var encoded = new byte[10];
-            //var count = HuffmanDecodingTests.Encode(original, encoded);
-            //var dst = new byte[10];
-            //var result = Huffman.Decode(encoded, 0, count, dst);
+            var source = new byte[6];
+            var destination = new byte[24];
 
-            // send single EOS
-            var EOS = new byte[] { 0b11111111, 0b11111111, 0b11111111, 0b11111100 };
-            //var source = new byte[1];
-            var destination = new byte[12];
-            for (int i = 0; i < 256; i++)
+            for (int i = 0; i < 255; i++)
             {
-                var encoded = HuffmanDecodingTests.GetEncodedValue((byte)i);
-                
-                //source[0] = (byte)i;
-                //int encodedByteCount = Encode(source, destination);
+                ulong currentBits = 0;  // We can have 7 bits of rollover plus 30 bits for the next encoded value, so use a ulong
+                int currentBitCount = 0;
+                int dstOffset = 0;
+
+                ulong code = 0;
+                int bitLength = 0;
+                for (int j = 0; j < 3; j++)
+                {
+                    if (j == 1)
+                    {
+                        // hard-coded EOS on the second pass
+                        code = (ulong)(0b11111111_11111111_11111111_11111100) << 32;
+                        bitLength = 30;
+                    }
+
+                    else
+                        (code, bitLength) = HuffmanDecodingTests.GetEncodedValue(source[0]);
+
+                    currentBits |= code >> currentBitCount;
+                    currentBitCount += bitLength;
+
+                    while (currentBitCount >= 8)
+                    {
+                        destination[dstOffset++] = (byte)(currentBits >> 56);
+                        currentBits = currentBits << 8;
+                        currentBitCount -= 8;
+                    }
+                }
+
+                // Fill any trailing bits with ones, per RFC
+                if (currentBitCount > 0)
+                {
+                    currentBits |= 0xFFFFFFFFFFFFFFFF >> currentBitCount;
+                    destination[dstOffset++] = (byte)(currentBits >> 56);
+                }
+
+                //yield return new object[] { destination.Take(dstOffset).ToArray() };
+                var dst = new byte[24];
+                //Assert.Throws<HuffmanDecodingException>(() => Huffman.Decode(destination.Take(dstOffset).ToArray(), dst));
             }
 
             Console.WriteLine("Finished");
